1976ba913ad32f6f8de09b60fe352f266897f894
meek0 <cedricgfont@gmail.com>
2021-02-18 14:05:53 -0500

    range col

--- src/components/panel/RqlPanelVocabulary.vue
+++ src/components/panel/RqlPanelVocabulary.vue
@@ -22,11 +22,11 @@
 
   <template v-else-if="criterion.type === 'NUMERIC'">
 
-    <div class="form-group">
+    <div class="form-group col-6 d-inline-block">
       <label v-bind:for="vocabulary.name + 'from'">{{ "search.from" | translate }}</label>
       <input type="number" class="form-control" v-bind:id="vocabulary.name + '-from'" v-model="criterion.value[0]" v-on:change="onInput()">
     </div>
-    <div class="form-group">
+    <div class="form-group col-6 d-inline-block">
       <label v-bind:for="vocabulary.name + 'to'">{{ "search.to" | translate }}</label>
       <input type="number" class="form-control" v-bind:id="vocabulary.name + '-to'" v-model="criterion.value[1]" v-on:change="onInput()">
     </div>
